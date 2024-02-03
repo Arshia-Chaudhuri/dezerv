c73943513df673bf57ddaa8e7142cdf744270b1a
Arshia-Chaudhuri <arshiachaudhuri99@gmail.com>
2024-02-03 22:26:05 +0530

    file updated

--- functionalities/studentsWithHighestMarks.py
+++ functionalities/studentsWithHighestMarks.py
@@ -2,4 +2,7 @@ from flask import Flask, request, jsonify
 from utils.db import db, Student, Course, Enrollment
 
 def students_with_highest_marks(courseId):
-    pass
+    try:
+        pass
+    except:
+        return jsonify({'error': 'Something went wrong'}), 400
